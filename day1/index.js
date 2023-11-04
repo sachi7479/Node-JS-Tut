d924987d8ac7abf89f7e48b46344c8e9bfcda31e
sachi7479 <sachi747981@gamil.com>
2023-11-04 20:49:42 +0530

    second

--- day1/index.js
+++ day1/index.js
@@ -1,7 +1,7 @@
 // import a module
 // const fs  = require('fs') for importing all methods
-const fs  = require('fs').writeFileSync;
-console.log("how are you?")
-fs.writeFileSync("hello.txt","how are you?")
+const fs = require("fs").writeFileSync;
+console.log("how are you?");
+fs.writeFileSync("hello.txt", "how are you i am fine");
 
 // __filename
